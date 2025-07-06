9ea6618bb58c9920978d587f29dcb49715507a25
@LeopoldHock <leopold.hock@deutschebahn.com>
2025-07-06 12:15:54 +0200

    feat: Improve root page

--- apps/spuxx-client/src/features/main/pages/root.page.tsx
+++ apps/spuxx-client/src/features/main/pages/root.page.tsx
@@ -1,12 +1,18 @@
 import { SessionService } from '@/services/session';
+import { intl } from '@spuxx/js-utils';
+import { Container } from '@spuxx/solid';
 import { Component } from 'solid-js';
 
 export const RootPage: Component = () => {
   SessionService.protectRoute();
 
   return (
-    <div>
-      <h1>Root Page</h1>
-    </div>
+    <Container class="text-center">
+      <h1>
+        {intl('main.page.root.title', {
+          name: SessionService.session()?.given_name,
+        })}
+      </h1>
+    </Container>
   );
 };
